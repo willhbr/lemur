b1431674e21d86e3e559115bcb9d58ed3603bd48
Will Richardson <william.hamish@gmail.com>
2023-06-21 09:59:41 +0000

    Rewrite

--- src/lemur.cr
+++ src/lemur.cr
@@ -1,210 +1,171 @@
 require "./parsers"
-require "./json_flag"
 
 module Lemur
-  FLAGS      = {} of String => FlagSettable
-  AFTER_INIT = [] of Proc(Nil)
+  FLAGS = [] of Lemur::FlagConfig
+  @@args = [] of String
 
-  class FlagError < Exception
+  def self.args
+    @@args
   end
 
-  module FlagSettable
-    abstract def set_from_value(value : String)
-    abstract def is_set? : Bool
-    abstract def set_to_default!
+  class FlagException < Exception
   end
 
-  macro finished
-    {% for name, flag in Lemur::FLAGS %}
-      def self.{{ attr.id }}
-        @@{{ attr }}.value
+  module FlagConfig
+    abstract def name : String
+    abstract def on_flag(value : String)
+    abstract def description : String
+    abstract def finished
+  end
+
+  module ValueParsing(T)
+    def from_flag(value : String)
+      {% if T.union? %}
+        {% for t in T.union_types %}
+          {% unless t == Nil %}
+            last_error = nil
+            begin
+              return {{ t }}.from_flag(value)
+            rescue error
+              last_error = error
+            end
+          {% end %}
+        {% end %}
+        {% if T.nilable? %}
+          Nil.from_flag(value)
+          return
+        {% end %}
+        if error = last_error
+          raise last_error
+        end
+      {% else %}
+        T.from_flag(value)
+      {% end %}
+    end
+  end
+
+  module SingleFlagConfig(T)
+    include ValueParsing(T)
+    @value : T? = nil
+    @set = false
+
+    def on_flag(value : String)
+      if @set
+        raise Lemur::FlagException.new "single flag --#{self.name} passed multiple times"
       end
-    {% end %}
-  end
+      @set = true
+      @value = from_flag(value)
+    end
 
-  @@argv = [] of String
-  @@initialized = false
-
-  def self.argv(include_unknown = false) : Array(String)
-    init(include_unknown)
-    @@argv
-  end
-
-  def self.init(include_unknown = false)
-    return if @@initialized
-    @@argv.clear
-    errors = [] of Exception
-    escape_found = false
-    ARGV.each do |arg|
-      if escape_found
-        @@argv << arg
-        next
+    def finished
+      unless @set
+        @value = self.default
       end
+    end
+
+    abstract def default : T
+
+    def value : T
+      @value.not_nil!
+    end
+  end
+
+  module RepeatedFlagConfig(T)
+    include ValueParsing(T)
+    @value = Array(T).new
+
+    def on_flag(value : String)
+      @value << from_flag(value)
+    end
+
+    def value : T
+      @value.not_nil!
+    end
+  end
+
+  def self.init
+    flags_per_name = FLAGS.to_h { |f| {f.name, f} }
+    args = [] of String
+    failures = [] of Exception
+    ARGV.each_with_index do |arg, index|
       if arg == "--"
-        escape_found = true
-        next
+        args.concat(ARGV[(index + 1)...])
+        break
       end
       if arg.starts_with?("--") || arg.starts_with?('-')
-        key, _, value = arg.partition('=')
-        key = key.lchop?("--") || key.lchop('-')
-        if flag = FLAGS[key]?
+        name, _, value = arg.partition('=')
+        name = name.lchop?("--") || name.lchop('-')
+        if flag = flags_per_name[name]?
           begin
-            flag.set_from_value(value)
-          rescue error
-            errors << error
+            flag.on_flag(value)
+          rescue ex : Lemur::FlagException
+            failures << ex
           end
         else
-          if include_unknown
-            @@argv.push(arg)
-          else
-            errors << FlagError.new("Got unknown flag --#{key}")
-          end
+          failures << Lemur::FlagException.new("unknown flag: #{name}")
         end
-      else
-        @@argv.push(arg)
       end
     end
-    if Lemur::FLAGS["help"].is_set?
-      Lemur::FLAGS.keys.sort.each do |flag_name|
-        flag = Lemur::FLAGS[flag_name]
-        puts "  #{flag}. #{flag.docs}"
-      end
-      exit 127
-    end
-    FLAGS.each do |name, flag|
-      unless flag.is_set?
-        flag.set_to_default!
-      end
-      unless flag.is_set?
-        errors << FlagError.new("No value passed for required flag #{flag}")
+    FLAGS.each do |flag|
+      begin
+        flag.finished
+      rescue ex : Lemur::FlagException
+        failures << ex
       end
     end
-    if errors.any?
-      errors.each do |err|
-        puts err.message
-      end
-      raise FlagError.new("Lemur flag init failed.")
+    unless failures.empty?
+      STDERR.puts failures.map(&.message).join('\n')
+      exit 1
     end
-    @@initialized = true
-    AFTER_INIT.each do |callback|
-      callback.call
-    end
-    AFTER_INIT.clear
+    @@args = args
   end
 
-  def self.on_init(&block)
-    AFTER_INIT << block
-  end
-
-  def self.check_initialized!
-    unless @@initialized
-      raise "Lemur was used but not initialized. Make sure to call Lemur.init before accessing flags."
-    end
-  end
-
-  macro json_flag(name, type, docs, default = nil)
-    module Lemur
-      @@{{ name }} = Flag(JSONFlag({{ type }})).new(
-        {{ name.stringify }},
-        {{ docs }},
-        {% if default != nil %}
-          Proc({{ type }}).new { {{ default }} }
-        {% else %}
-          nil
-        {% end %})
-      Lemur::FLAGS[{{ name.stringify }}] = @@{{ name }}
-      def self.{{ name }} : {{ type }}
-        @@{{ name }}.value.wrapped
-      end
-    end
-  end
-
-  macro flag(name, type, docs, default = nil)
-    module Lemur
-      @@{{ name }} = Flag({{ type }}).new(
-        {{ name.stringify }},
-        {{ docs }},
-        {% if default != nil %}
-          Proc({{ type }}).new { {{ default }} }
-        {% else %}
-          nil
-        {% end %})
-      Lemur::FLAGS[{{ name.stringify }}] = @@{{ name }}
-      def self.{{ name }} : {{ type }}
-        @@{{ name }}.value
-      end
-    end
-  end
-end
-
-class Lemur::Flag(T)
-  include FlagSettable
-
-  getter docs
-  @value : T?
-  @set = false
-
-  def initialize(@name : String, @docs : String, @default : Proc(T)?)
-  end
-
-  def set_from_value(value)
-    {% if T.union? %}
-      {% for t in T.union_types %}
-        {% unless t == Nil %}
-          last_error = nil
-          begin
-            @value = {{ t }}.from_flag(value)
-            @set = true
-            return
-          rescue error
-            last_error = error
-          end
-        {% end %}
-      {% end %}
-      {% if T.nilable? %}
-        Nil.from_flag(value)
-        @set = true
-        return
-      {% end %}
-      if error = last_error
-        raise last_error
-      end
-    {% else %}
-      @value = T.from_flag(value)
-    {% end %}
-    @set = true
-  end
-
-  def value : T
-    Lemur.check_initialized!
-    {% if T.nilable? %}
-      @value
-    {% else %}
-      @value.not_nil!
-    {% end %}
-  end
-
-  def is_set? : Bool
-    @set
-  end
-
-  def set_to_default!
+  macro flag_internal(name, type, description, config, &block)
     {% begin %}
-      if default = @default
-        @set = true
-        @value = default.call
-      {% if T.nilable? %}
-      else
-        @set = true
-        @value = nil
-      {% end %}
+      class Lemur::Flag__{{ name }}%name
+        include {{ config }}({{ type }})
+        include Lemur::FlagConfig
+
+        def name : String
+          {{ name.stringify }}
+        end
+
+        def description : String
+          {{ description }}
+        end
+
+        {{ block.body }}
+      end
+
+      module Lemur
+        @@%flag = Lemur::Flag__{{ name }}%name.new
+        FLAGS << @@%flag
+        def self.{{ name }} : {{ type }}
+          @@%flag.value
+        end
       end
     {% end %}
   end
 
-  def to_s(io)
-    io << "--" << @name << " : " << T << " = " << @value.inspect
+  macro flag(name, type, description, default = nil)
+    Lemur.flag_internal(
+      {{ name }}, {{ type }}, {{ description }},
+      Lemur::SingleFlagConfig) do
+      def default : {{ type }}
+        {% if default == nil && !type.resolve.nilable? %}
+          raise Lemur::FlagException.new(
+            "missing required flag: --#{self.name} (#{self.description})")
+        {% else %}
+          {{ default }}
+        {% end %}
+      end
+    end
+  end
+
+  macro repeated_flag(name, type, description)
+    Lemur.flag_internal(
+      {{ name }}, {{ type }}, {{ description }},
+      Lemur::RepeatedFlagConfig) do
+    end
   end
 end
-
-Lemur.flag(help, Nil, "Show flag documentation")
